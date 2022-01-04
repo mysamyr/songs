07854026ffd4e6d5508f5848653d0e8ddd1b68c4
mysamyr <lubomirmisak@live.com>
2022-01-04 21:09:04 +0200

    add edit feature

--- routes/home.js
+++ routes/home.js
@@ -5,7 +5,7 @@ const router = Router();
 router.get('/', async (req, res) => {
   const songs = await Songs.find().select("name id");
   res.render('index', {
-    title: 'Home page',
+    title: "Збірник пісень",
     songs: songs.map(i => i.toObject())
   })
 });
--- routes/song.js
+++ routes/song.js
@@ -5,7 +5,7 @@ const router = Router();
 router.get("/:id", async (req, res) => {
   const song = await Songs.findOne({_id: req.params.id});
   res.render('song', {
-    title: 'Home page',
+    title: song.name,
     name: song.name,
     author: song.author,
     text: song.text,
@@ -17,6 +17,30 @@ router.post("/:id", async (req, res) => {
   const song = await Songs.findOne({_id: req.params.id});
   song.deleteOne();
   res.redirect("/");
+});
+
+router.get("/edit/:id", async (req, res) => {
+  const song = await Songs.findOne({_id: req.params.id});
+  res.render("edit", {
+    title: "Редагування",
+    name: song.name,
+    author: song.author,
+    text: song.text,
+    id: song.id
+  });
+});
+
+router.post("/edit/:id", async (req, res) => {
+  try {
+    await Songs.findOneAndUpdate({_id: req.params.id}, {
+      name: req.body.name,
+      text: req.body.text,
+      author: req.body.author,
+    });
+    res.redirect(`/song/${req.params.id}`)
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 module.exports = router;
