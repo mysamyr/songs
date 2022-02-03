47a5be83d3edd57265275d7ca1d00fdd748bd1eb
mysamyr <lubomirmisak@live.com>
2022-02-03 10:36:24 +0200

    refactoring custom script

--- public/app.js
+++ public/app.js
@@ -1,41 +1,48 @@
-M.Tabs.init(document.querySelectorAll(".tabs"));
+document.addEventListener('DOMContentLoaded', function () {
+  try {
+    // tabs for auth
+    M.Tabs.init(document.querySelectorAll(".tabs"));
 
-document.addEventListener('DOMContentLoaded', function() {
-  // flash events
-  const flashAlert = document.querySelector(".alert");
-  const flashMsg = document.querySelector(".msg");
-  if (flashAlert) {
-    setTimeout(() => {
-      flashAlert.remove();
-    }, 3000);
+    // beauty select view
+    M.FormSelect.init(document.querySelectorAll('select'));
+
+    // sidebar for mobile
+    M.Sidenav.init(document.querySelectorAll('.sidenav'));
+
+    // flash events
+    const flashAlert = document.querySelector(".alert"),
+      flashMsg = document.querySelector(".msg");
+    if (flashAlert) {
+      setTimeout(() => {
+        flashAlert.remove();
+      }, 3000);
+    }
+    if (flashMsg) {
+      setTimeout(() => {
+        flashMsg.remove();
+      }, 3000);
+    }
+
+    // liturgy view
+    const liturgyText = document.querySelector("#lit");
+    if (liturgyText) {
+      const container = document.querySelector(".container");
+
+      container.innerHTML = liturgyText.getAttribute("data-text");
+
+      liturgyText.remove();
+
+      const buttonWrapper = document.createElement("div");
+      buttonWrapper.className = "buttons";
+      buttonWrapper.innerHTML = `
+        <form action="/" method="GET">
+            <button class="btn goHome">На головну</button>
+        </form>
+    `;
+
+      container.append(buttonWrapper)
+    }
+  } catch (e) {
+    console.log(e);
   }
-  if (flashMsg) {
-    setTimeout(() => {
-      flashMsg.remove();
-    }, 3000);
-  }
-  // liturgy
-  const liturgyText = document.querySelector("#lit");
-   if (liturgyText) {
-     const container = document.querySelector(".container");
-
-     container.innerHTML = liturgyText.getAttribute("data-text");
-
-     liturgyText.remove();
-
-     const button = document.createElement("div");
-     button.className = "buttons";
-     button.innerHTML = `<form action="/" method="GET">
-        <button class="btn goHome">На головну</button>
-    </form>`;
-
-     container.append(button)
-   }
-  // beauty select view
-  const elems = document.querySelectorAll('select');
-  M.FormSelect.init(elems);
-
-  // sidebar for mobile
-  const sidebar = document.querySelectorAll('.sidenav');
-  M.Sidenav.init(sidebar);
 });
